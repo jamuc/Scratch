200b363740153d4e642031f96a7df079192c787e
Jason Franklin <jason.b.franklin@gmail.com>
2014-06-21 22:55:53 +0200

    Dynamically define missing methods
    - Check for getter/setter sytax, otherwise call super on method missing
    - Define methods at run time, to minimize call stack

--- MyStruct/my_struct.rb
+++ MyStruct/my_struct.rb
@@ -3,14 +3,39 @@
 class MyStruct
 
     def initialize(opts={})
-        @values = opts
+        @values = Hash.new
+        opts.each do |key, value|
+            generate_accessors(key, value)
+        end
     end
 
     def method_missing(method_name, *args, &block)
         setter_reg = /(\w+)\s{0,1}=$/
-        match = method_name.to_s.match(setter_reg)
-        @values[match[1].to_sym] = args.first if match
+        getter_reg = /(\w+)$/
         
-        @values[method_name]
+        match = method_name.to_s.match(setter_reg)
+        if match
+            method_name = match[1].to_sym
+        else
+            match = method_name.to_s.match(getter_reg)
+            super unless match
+        end
+
+        generate_accessors(method_name, args.first)
+        self.send(method_name)
+    end
+
+private
+
+    def generate_accessors(method_name, arg)
+        setter_method_name = (method_name.to_s + "=").to_sym
+        
+        self.class.send :define_method, setter_method_name do |value|
+            @values[method_name] = value
+        end
+        self.class.send :define_method, method_name do
+            @values[method_name]
+        end
+        self.send(setter_method_name, arg)
     end
 end
