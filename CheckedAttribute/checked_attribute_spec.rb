e94a6767c65654d2bdb4704327330db2b3e5519b
Jason Franklin <jason.b.franklin@gmail.com>
2014-07-19 16:30:06 +0200

    Implement add_checked_attribute method

--- CheckedAttribute/checked_attribute_spec.rb
+++ CheckedAttribute/checked_attribute_spec.rb
@@ -4,6 +4,17 @@ class Person
 end
 
 def add_checked_attribute(klass, attribute)
+    klass.class_eval do
+        define_method attribute do 
+            instance_variable_get "@#{attribute}"
+        end
+    
+        define_method "#{attribute}=" do |new_age|
+            raise 'Invalid attribute' unless new_age
+
+            instance_variable_set("@#{attribute}", new_age)
+        end
+    end
 end
 
 describe Person do 
